1f4d0700b18b39832fa38849faab9167831bd2f1
bbogeul <illumeweb@gmail.com>
2020-02-12 19:57:09 +0900

    set up main swagger builder

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { json, urlencoded } from 'body-parser';
 import * as compression from 'compression';
 import * as helmet from 'helmet';
 import * as requestIp from 'request-ip';
+import * as packageInfo from '../package.json';
+import { ClassTransformOptions } from '@nestjs/common/interfaces/external/class-transform-options.interface';
 
 let app: NestExpressApplication;
 declare const module: any;
@@ -26,5 +28,45 @@ async function bootstrap() {
   app.use(compression());
   app.use(helmet()); // https://helmetjs.github.io/
   app.use(requestIp.mw());
+
+  // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, HaProxy, etc)
+  // see https://expressjs.com/en/guide/behind-proxies.html
+  app.set('trust proxy', process.env.NODE_ENV !== 'development');
+
+  // app.setGlobalPrefix(BASE_PREFIX_PATH);
+  // console.log('process - env ', process.env);
+
+  // Validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // skipMissingProperties: true,
+      // skipNullProperties: true,
+      // skipUndefinedProperties: false,
+      validationError: { target: false, value: false }, // object 와 value 역전송 막기
+      transform: true,
+      transformOptions: {
+        excludeExtraneousValues: true,
+      } as ClassTransformOptions, // version문제로 실제 있지만 여기 없음.. down casting
+    }),
+  );
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  // swagger options
+  if (process.env.NODE_ENV === 'development') {
+    const options = new DocumentBuilder()
+      .setTitle(packageInfo.name.toUpperCase())
+      .setDescription('NestJs Mongoose Version')
+      .setVersion(packageInfo.version)
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('swagger', app, document);
+  }
 }
 bootstrap();
